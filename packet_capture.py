eb535c60e5535666103a6fd1e68e0d7590926b82
KnightRipper <arnavamol77@gmail.com>
2024-09-05 12:57:45 +0530

    Update packet_capture.py

--- packet_capture.py
+++ packet_capture.py
@@ -1,1 +1,10 @@
+from scapy.all import sniff, IP
 
+def packet_callback(packet):
+    if IP in packet:
+        ip_src = packet[IP].src
+        ip_dst = packet[IP].dst
+        protocol = packet[IP].proto
+        
+        print(f"Packet: {ip_src} -> {ip_dst} | Protocol: {protocol}")
+sniff(filter="ip", prn=packet_callback, store=0)
