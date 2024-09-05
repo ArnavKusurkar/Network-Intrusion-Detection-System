32189059d76f587afd0954def37e80dfbed8ad23
KnightRipper <arnavamol77@gmail.com>
2024-09-05 13:00:10 +0530

    Update anamoly_detection.py

--- anamoly_detection.py
+++ anamoly_detection.py
@@ -1,1 +1,17 @@
+from scapy.all import sniff, IP
+from collections import defaultdict
+
+bandwidth_usage = defaultdict(int)
+
+def detect_bandwidth_anomaly(packet):
+    if IP in packet:
+        ip_src = packet[IP].src
+        packet_size = len(packet)
+
+        bandwidth_usage[ip_src] += packet_size
+        
+        if bandwidth_usage[ip_src] > 1000000:  
+            print(f"Bandwidth anomaly detected from {ip_src} with {bandwidth_usage[ip_src]} bytes in usage")
+
+sniff(filter="ip", prn=detect_bandwidth_anomaly, store=0)
 
